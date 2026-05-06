361f9f92112d46f3c7d0d03f38bc288ed973dd5b
Julio <julio.nascimento@sipam.gov.br>
2026-05-06 13:15:50 -0400

    praticando Java e Estruturas e paradigmas de orientação a objetos

---
+++ praticando/src/Logica/PraticarLogica.java
@@ -0,0 +1,34 @@
+package Logica;
+
+import java.lang.*;
+public class PraticarLogica {
+   public static void main(String[] args) {
+
+//       int i = 100;
+//       double d = i;
+//
+//       double d1 = 10.5;
+//       int i2 =(int) 10.5;
+
+
+
+
+        for(int i =0; i<5; i++ ){
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
